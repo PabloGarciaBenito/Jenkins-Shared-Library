1bb210a11a603f47903969091b0fcec409868a40
PABLO GARCIA BENITO <6002859@viewnext.com>
2024-02-22 10:24:23 +0100

    eliminada variable sobrante

--- vars/resultados.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env Groovy
-
-def call(){
-     junit 'java-app/target/surefire-reports/TEST-*.xml'
-     archiveArtifacts artifacts: 'java-app/target/surefire-reports/*.xml', fingerprint: true
-}
