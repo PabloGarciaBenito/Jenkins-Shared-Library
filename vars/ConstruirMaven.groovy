8c7ec41f43e6b3bca2e4f5606b028e0fb3044272
PABLO GARCIA BENITO <6002859@viewnext.com>
2024-02-22 09:15:00 +0100

    variables arregladas

--- vars/ConstruirMaven.groovy
+++ vars/ConstruirMaven.groovy
@@ -2,6 +2,6 @@
 
 def call(){
     sh '''
-    docker run --rm -v /home/jenkins/jenkins/jenkins_home/workspace/EjercicioB5/java-app:/app -v /var/jenkins_home/.m2:/root/.m2 -w /app maven:3-alpine mvn -B -DskipTests clean package
+    docker run --rm -v /home/jenkins/jenkins/jenkins_home/workspace/EjercicioB5@libs/356783fcc28c80aec0c283bed886a5fb125ccbbb5f33642dd251b4a6c2fe6733/java-app:/app -v /var/jenkins_home/.m2:/root/.m2 -w /app maven:3-alpine mvn -B -DskipTests clean package
     '''
 }
