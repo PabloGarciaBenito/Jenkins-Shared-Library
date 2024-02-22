5da7dfd02b450f6fbc56e8ae858dc0495413924c
PABLO GARCIA BENITO <6002859@viewnext.com>
2024-02-22 10:01:52 +0100

    UnitTest arreglado

--- vars/UnitTest.groovy
+++ vars/UnitTest.groovy
@@ -2,6 +2,7 @@
 
 def call(){
     sh '''
-    docker run --rm -v /home/jenkins/jenkins/jenkins_home/workspace/Pipeline-Shared-Library/java-app:/app -v /var/jenkins_home/.m2:/root/.m2 -w /app maven:3-alpine mvn test
+    cd ../Pipeline-Shared-Library@libs/356783fcc28c80aec0c283bed886a5fb125ccbbb5f33642dd251b4a6c2fe6733/java-app/ && /
+    mvn test
     '''
 }
