4a3f44d3902b51a3df2e34b7c8360c158bf15d81
PABLO GARCIA BENITO <6002859@viewnext.com>
2024-02-22 10:18:54 +0100

    modificadas variables

--- vars/UnitTest.groovy
+++ vars/UnitTest.groovy
@@ -2,7 +2,7 @@
 
 def call(){
     sh '''
-    cd ../Pipeline-Shared-Library@libs/356783fcc28c80aec0c283bed886a5fb125ccbbb5f33642dd251b4a6c2fe6733/java-app/ && /
+    cd java-app/ && /
     mvn test
     '''
 }
