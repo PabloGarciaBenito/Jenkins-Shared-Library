c2e1dc3793f61fd928db9f8bdc26d2fa31b529c8
PABLO GARCIA BENITO <6002859@viewnext.com>
2024-02-22 08:40:32 +0100

    Primer commit

---
+++ vars/ConstruirMaven.groovy
@@ -0,0 +1,7 @@
+#!/usr/bin/env groovy
+
+def call(){
+    sh '''
+    docker run --rm -v /home/jenkins/jenkins/jenkins_home/workspace/EjercicioB5/java-app:/app -v /var/jenkins_home/.m2:/root/.m2 -w /app maven:3-alpine mvn -B -DskipTests clean package
+    '''
+}
